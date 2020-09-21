8999ade8a5409fe66b4e6d80d57b253575801f41
Felipe Pino <felipepinoredes@gmail.com>
2020-09-21 16:22:13 -0400

    no funciona migracion

---
+++ resources/js/components/MyThoughtsComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            
+            <form-component @new="addThought"></form-component>
+            
+            <thought-component 
+                v-for="(thought, index) in thoughts" 
+                :key="thought.id"
+                :thought="thought"
+                @update="updateThought(index, ...arguments)"
+                @delete="deleteThought(index)">
+            </thought-component>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                thoughts:[{
+                    'id':1,
+                    'description':'abc',
+                    'created_at':'20/09/2020'
+                }]
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods: {
+            addThought(thought) {
+                this.thoughts.push(thought);
+            },
+            updateThought(index) {
+                this.thoughts[index] = thought;
+            },
+            deleteThought(index) {
+                this.thoughts.splice(index, 1);
+            }
+        }
+    }
+</script>
